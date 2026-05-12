0e378a6f97a3135a68af4d5f8df9f8c5d5f62ff7
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-12 15:21:16 +0200

    Oprettet CustomerMapper med saveCustomer()

---
+++ src/main/java/app/persistence/CustomerMapper.java
@@ -0,0 +1,33 @@
+package app.persistence;
+
+import app.app.exceptions.DatabaseException;
+import app.entities.Customer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CustomerMapper {
+
+    public void saveCustomer(Customer customer, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "insert into customers (first_name, last_name, address, zipcode, email) values (?, ?, ?, ?, ?)";
+
+        try ( Connection connection = connectionPool.getConnection();
+              PreparedStatement ps = connection.prepareStatement(sql);
+        ) {
+            ps.setString(1, customer.getFirstname());
+            ps.setString(2, customer.getLastname());
+            ps.setString(3, customer.getAddress());
+            ps.setInt(4, customer.getZipcode().getZipcode());
+            ps.setString(5, customer.getEmail());
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved oprettelse af kunde.");
+            }
+        } catch (SQLException e) {
+            String msg = "Fejl ved ...";
+            throw new DatabaseException(msg, e.getMessage());
+        }
+    }
+}
