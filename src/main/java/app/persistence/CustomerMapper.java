edcbc12d571cff1ec4e7f2a84252a716e4303df2
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-13 15:32:12 +0200

    CustomerMapper: indsat kommentar til getZipcode(), rettet SQL-statements til store bogstaver

--- src/main/java/app/persistence/CustomerMapper.java
+++ src/main/java/app/persistence/CustomerMapper.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class CustomerMapper {
 
     public void saveCustomer(Customer customer, ConnectionPool connectionPool) throws DatabaseException {
-        String sql = "insert into customers (first_name, last_name, address, zipcode, email) values (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO customers (first_name, last_name, address, zipcode, email) VALUES (?, ?, ?, ?, ?)";
 
         try ( Connection connection = connectionPool.getConnection();
               PreparedStatement ps = connection.prepareStatement(sql);
@@ -18,7 +18,7 @@ public class CustomerMapper {
             ps.setString(1, customer.getFirstname());
             ps.setString(2, customer.getLastname());
             ps.setString(3, customer.getAddress());
-            ps.setInt(4, customer.getZipcode().getZipcode());
+            ps.setInt(4, customer.getZipcode().getZipcode()); //Første getZipcode() henter Zipcode-objekt fra kunden, anden getZipcode() henter zipcode fra Zipcode-objekt.
             ps.setString(5, customer.getEmail());
 
             int rowsAffected = ps.executeUpdate();
