e9a10b50257091f64a7c0e3ac5effbb1b0a251f4
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-14 13:06:22 +0200

    QuoteController: Oprettet controller med tom addRoutes()

---
+++ src/main/java/app/controllers/QuoteController.java
@@ -0,0 +1,11 @@
+package app.controllers;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
+public class QuoteController {
+
+    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
+
+    }
+}
