43a8af5b94fa008ed7ef2ee4cc74d208511c6cf5
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-14 13:06:35 +0200

    SalespersonController: Oprettet controller med tom addRoutes()

---
+++ src/main/java/app/controllers/SalespersonController.java
@@ -0,0 +1,11 @@
+package app.controllers;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
+public class SalespersonController {
+
+    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
+
+    }
+}
