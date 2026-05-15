b4fd903ffae7794e11fb9abfd0f9c82e30753ae2
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-15 14:16:55 +0200

    QuoteService: Tilføjet createQuote() og quoteMapper

--- src/main/java/app/services/QuoteService.java
+++ src/main/java/app/services/QuoteService.java
@@ -1,4 +1,15 @@
 package app.services;
 
+import app.entities.Quote;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.QuoteMapper;
+
 public class QuoteService {
+
+    private QuoteMapper quoteMapper = new QuoteMapper();
+
+    public int createQuote(Quote quote, ConnectionPool connectionPool) throws DatabaseException {
+        return quoteMapper.createQuote(quote, connectionPool);
+    }
 }
