f6b5aab59b96337c5b19f4b5b3d820e7322996b4
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-15 14:17:30 +0200

    Salesperson: Tilføjet ekstra konstruktør

--- src/main/java/app/entities/Salesperson.java
+++ src/main/java/app/entities/Salesperson.java
@@ -9,6 +9,15 @@ public class Salesperson {
     private String password;
     private String role;
 
+    public Salesperson(int salespersonId, String firstName, String lastName, String email, String password, String role) {
+        this.salespersonId = salespersonId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
     public Salesperson(String firstName, String lastName, String email, String password, String role) {
         this.firstName = firstName;
         this.lastName = lastName;
