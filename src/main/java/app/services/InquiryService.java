7876fc0e51201321e744c5005212c1bc8f83cb1d
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-15 14:13:31 +0200

    InquiryService: Tilføjet getAllInquiries() og getInquiryById()

--- src/main/java/app/services/InquiryService.java
+++ src/main/java/app/services/InquiryService.java
@@ -5,15 +5,12 @@ import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.InquiryMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InquiryService {
 
     private InquiryMapper inquiryMapper = new InquiryMapper();
-
-
-
-
 
     public void handleInquiry(Inquiry inquiry, ConnectionPool connectionPool) throws DatabaseException{
         //forretnings logik put her ind.
@@ -21,7 +18,11 @@ public class InquiryService {
          inquiryMapper.saveInquiry(inquiry, connectionPool);
     }
 
+    public List<Inquiry> getAllInquiries(ConnectionPool connectionPool) throws DatabaseException {
+        return inquiryMapper.getAllInquiries(connectionPool);
+    }
 
-
-
+    public Inquiry getInquiryById(int inquiryId, ConnectionPool connectionPool) throws DatabaseException {
+        return inquiryMapper.getInquiryBy(inquiryId, connectionPool);
+    }
 }
