547656ce116592e90301e24579da5f0418aff73f
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-16 18:36:41 +0200

    CustomerMapper: Ændret returtypen for saveCustomer()

--- src/main/java/app/persistence/CustomerMapper.java
+++ src/main/java/app/persistence/CustomerMapper.java
@@ -3,17 +3,15 @@ package app.persistence;
 import app.exceptions.DatabaseException;
 import app.entities.Customer;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class CustomerMapper {
 
-    public void saveCustomer(Customer customer, ConnectionPool connectionPool) throws DatabaseException {
+    public int saveCustomer(Customer customer, ConnectionPool connectionPool) throws DatabaseException {
         String sql = "INSERT INTO customers (first_name, last_name, address, zipcode, email) VALUES (?, ?, ?, ?, ?)";
 
-        try ( Connection connection = connectionPool.getConnection();
-              PreparedStatement ps = connection.prepareStatement(sql);
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
         ) {
             ps.setString(1, customer.getFirstname());
             ps.setString(2, customer.getLastname());
@@ -21,10 +19,12 @@ public class CustomerMapper {
             ps.setInt(4, customer.getZipcode().getZipcode()); //Første getZipcode() henter Zipcode-objekt fra kunden, anden getZipcode() henter zipcode fra Zipcode-objekt.
             ps.setString(5, customer.getEmail());
 
-            int rowsAffected = ps.executeUpdate();
-            if (rowsAffected != 1) {
-                throw new DatabaseException("Fejl ved oprettelse af kunde.");
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
             }
+            throw new DatabaseException("Fejl ved oprettelse af kunde.");
         } catch (SQLException e) {
             String msg = "Fejl ved ...";
             throw new DatabaseException(msg, e.getMessage());
