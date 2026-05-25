4d28bf20d360193deeb86b36f766a19a7cd7f769
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-25 15:24:00 +0200

    Main: Tilføjet midlertidig testrute. Skal slettes inden merge!

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,9 +3,14 @@ package app;
 import app.controllers.InquiryController;
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
+import app.controllers.QuoteController;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.services.CarportSvg;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
+
+import java.util.Locale;
 
 public class Main {
 
@@ -25,6 +30,22 @@ public class Main {
 
         InquiryController inquiryController = new InquiryController();
         inquiryController.addRoutes(app, connectionPool);
+
+        QuoteController quoteController = new QuoteController();
+        quoteController.addRoutes(app, connectionPool);
+
+
+//TODO MIDLERTIDIG TESTRUTE - SLET EFTER BRUG
+        app.get("/quote", ctx -> {
+            try {
+                Locale.setDefault(new Locale("US"));
+                CarportSvg carportSvg = new CarportSvg(540, 600, connectionPool);
+                ctx.attribute("svg", carportSvg.toString());
+                ctx.render("quote.html");
+            } catch (DatabaseException e) {
+                ctx.result(e.getMessage());
+            }}
+        );
 
     }
 }
