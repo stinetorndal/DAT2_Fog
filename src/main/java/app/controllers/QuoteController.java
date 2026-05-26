f12536f9b220692c3f27213fe3f46cce35e31d22
KatarinaKN <katarinak.nielsen@gmail.com>
2026-05-26 12:02:37 +0200

    QuoteController: to branches merget sammen.

--- src/main/java/app/controllers/QuoteController.java
+++ src/main/java/app/controllers/QuoteController.java
@@ -25,35 +25,35 @@ public class QuoteController {
     }
 
     private void createQuote(Context ctx, ConnectionPool connectionPool) {
-    private void viewAllQuotations (Context ctx, ConnectionPool connectionPool) {
-        try {
-            int inquiryId = Integer.parseInt(ctx.pathParam("id"));
-            Inquiry inquiry = inquiryService.getInquiryById(inquiryId, connectionPool);
-            Salesperson salesperson = ctx.sessionAttribute("currentUser");
-            List<Quote> quotes = quoteService.handleAllQuotes(connectionPool);
+        private void viewAllQuotations (Context ctx, ConnectionPool connectionPool){
+            try {
+                int inquiryId = Integer.parseInt(ctx.pathParam("id"));
+                Inquiry inquiry = inquiryService.getInquiryById(inquiryId, connectionPool);
+                Salesperson salesperson = ctx.sessionAttribute("currentUser");
+                List<Quote> quotes = quoteService.handleAllQuotes(connectionPool);
 
-            if (salesperson != null) {
-                int salespersonId = salesperson.getSalespersonId();
-                int length = inquiry.getCarportLength();
-                int width = inquiry.getCarportWidth();
-                double quotePrice = calculateTotalPrice.calculatePrice(inquiry, connectionPool);
-                int quotationNumber = quoteService.getQuotationNumber(connectionPool);
-                Quote quote = new Quote(inquiryId, salespersonId, length, width, quotePrice, quotationNumber);
-                quoteService.createQuote(quote, connectionPool);
-                ctx.redirect("/sales/all-inquiries"); //redirect() fordi ellers vil url'en stadig vise url'en til den enkelte forespørgsel.
-            } else {
-                ctx.redirect("/login");
+                if (salesperson != null) {
+                    int salespersonId = salesperson.getSalespersonId();
+                    int length = inquiry.getCarportLength();
+                    int width = inquiry.getCarportWidth();
+                    double quotePrice = calculateTotalPrice.calculatePrice(inquiry, connectionPool);
+                    int quotationNumber = quoteService.getQuotationNumber(connectionPool);
+                    Quote quote = new Quote(inquiryId, salespersonId, length, width, quotePrice, quotationNumber);
+                    quoteService.createQuote(quote, connectionPool);
+                    ctx.redirect("/sales/all-inquiries"); //redirect() fordi ellers vil url'en stadig vise url'en til den enkelte forespørgsel.
+                } else {
+                    ctx.redirect("/login");
+                }
+            } catch (DatabaseException | NumberFormatException e) {
+                ctx.attribute("message", e.getMessage());
+                ctx.redirect("/sales/all-inquiries");
+                //Så Thymeleaf kan læse den
+                ctx.attribute("alleTilbud", quotes);
+                ctx.render("sales_quotations.html");
+            } catch (DatabaseException e) {
+                ctx.attribute("message", "Kunne ikke hente alle tilbud " + e.getMessage());
+                ctx.render("sales_dashboard.html");
             }
-        } catch (DatabaseException | NumberFormatException e) {
-            ctx.attribute("message", e.getMessage());
-            ctx.redirect("/sales/all-inquiries");
-            //Så Thymeleaf kan læse den
-            ctx.attribute("alleTilbud", quotes);
-            ctx.render("sales_quotations.html");
-        } catch (DatabaseException e) {
-            ctx.attribute("message", "Kunne ikke hente alle tilbud " + e.getMessage());
-            ctx.render("sales_dashboard.html");
         }
     }
-}
 }
