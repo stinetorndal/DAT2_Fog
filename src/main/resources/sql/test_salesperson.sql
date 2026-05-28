1b2b7d052c4497a51bef08054478a0ea3f10016c
stinetorndal <cph-sc95@stud.ek.dk>
2026-05-28 10:42:25 +0200

    Ny testkunde lavet

--- src/main/resources/sql/test_salesperson.sql
+++ src/main/resources/sql/test_salesperson.sql
@@ -2,4 +2,4 @@ INSERT INTO public.salespersons (first_name, last_name, email, password, role) V
     ('Martin', 'Kristensen', 'salg@fog.dk', 'NytPassword26', 'salg');
 
 INSERT INTO public.customers (first_name, last_name, address, zipcode, email) VALUES
-    ('Test', 'Kunde', 'TestVej 123', '4000', 'test@email.dk');
+    ('Test', 'Kunde', 'Roskildevej 123', '4000', 'test@email.dk');
